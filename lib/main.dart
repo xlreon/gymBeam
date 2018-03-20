f47e5d09e770a83de42c6f17e8ee27d7982b950c
Sidharth <sidharth.satapathy9@gmail.com>
2018-03-21 02:55:32 +0530

    Made new class HomePage and handle navigation of Profile button.

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,42 @@ class GymBeam extends StatelessWidget {
     accentColor: Colors.blue,
   );
 
-  final List toolbarItems = [new BottomNavigationBarItem(
-                icon: new Icon(
-                  Icons.home
-                ),
-                title: new Text("home")
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Gym Beam',
+      routes: <String, WidgetBuilder>{
+        '/profile': (BuildContext context) => new ProfilePage(),
+      },
+      // Theme setting for IOS and android
+      theme: defaultTargetPlatform == TargetPlatform.iOS
+            ? kIOSTheme
+            : kDefaultTheme,
+      home : new HomePage()
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Profile'),
+        ),
+        body: new Center(
+            child: new Text('Profile Screen')
+        )
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+    final List toolbarItems = [
+            new BottomNavigationBarItem(        
+            icon: new Icon(Icons.home),
+            title: new Text("home"),
             ),
             new BottomNavigationBarItem(
               icon: new Icon(Icons.search),
@@ -30,18 +61,15 @@ class GymBeam extends StatelessWidget {
               title: new Text("money")
             )];
 
-
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Gym Beam',
-      // Theme setting for IOS and android
-      theme: defaultTargetPlatform == TargetPlatform.iOS
-            ? kIOSTheme
-            : kDefaultTheme,
-      home : new Scaffold(
+    return new Scaffold(
         appBar: new AppBar(
           title: new Text("Gym Beam"),
+          actions: <Widget>[
+            new IconButton(icon: new Icon(Icons.perm_identity,color: Colors.white), onPressed: () => Navigator.of(context).pushNamed('/profile')
+            )
+          ],
         ),
       body: new Scaffold(
         bottomNavigationBar: Theme.of(context).platform == TargetPlatform.iOS
@@ -50,15 +78,16 @@ class GymBeam extends StatelessWidget {
           key: new Key("toolbar"),
           // Toolbar menu items
           items: toolbarItems,
+          currentIndex: 1,
         )
         : // Android theme for toolbar
           new BottomNavigationBar(
           key: new Key("toolbar"),
           // Toolbar menu items
           items: toolbarItems,
+          currentIndex: 1,
         )
         ,
-      )
       )
     );
   }
