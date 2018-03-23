38156f30d73bb24cea7594ac5fa5c94a9afb9519
Rohan Panda <rohan.panda1@gmail.com>
2018-03-24 03:55:22 +0530

    searchBox onTap fixed

--- lib/components/homeBanner.dart
+++ lib/components/homeBanner.dart
@@ -61,25 +61,34 @@ class BannerState extends State<BannerComponent> {
                 _buildProfileIcon(),
                 new Container(
                   child: new GestureDetector(
-                    onTap: () => Navigator.of(context).pushNamed('/search'),
+                    onTap: ()  {print ("SearchBox tapped.");Navigator.of(context).pushNamed('/search');},
                     child: new Container(
                       padding: new EdgeInsets.only(
                         top: 90.0, left: 25.0, right: 25.0, bottom: 0.0
                       ),
-                      child: new TextField(
-                        controller:  _textController,
-                        decoration: new InputDecoration(
-                          hintText: "Search Gym nearby you",
-                          prefixIcon: new IconButton(
+                      child:new Container(
+                        height: 50.0,
+                        // margin: new EdgeInsets.symmetric(horizontal:10.0),
+                        decoration: new BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: const BorderRadius.all(const Radius.circular(30.0)),
+                          border: new Border.all(
+                            color: Colors.grey,
+                            width: 2.0,
+                          ),
+                        ),
+                        child: new Row(children: <Widget>[
+                          new IconButton(
                             icon: new Icon(Icons.search),
                             // onPressed: () => Navigator.of(context).pushNamed('/search'),
                             iconSize: 25.0,
                           ),
-                          border: new OutlineInputBorder(borderRadius: const BorderRadius.all(const Radius.circular(30.0))),
-                          // filled: true,
-                          // fillColor: Colors.white,
-                        ),
-                      ),
+                          new Text(
+                            "Enter Gym nearby you",
+                            style: new TextStyle(fontWeight: FontWeight.w100),
+                          )
+                        ],),
+                      )
                     ),
                   ),
                 ),
