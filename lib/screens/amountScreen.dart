9fde9ebb7216cbd19e97718a9b7b8cccb3f6d438
Rohan Panda <rohan.panda1@gmail.com>
2018-05-03 14:57:53 +0530

    dateTime Screen done

---
+++ lib/screens/amountScreen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class AmountScreen extends StatefulWidget {
+  @override
+  AmountScreenState createState() => new AmountScreenState();
+}
+
+class AmountScreenState extends State<AmountScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text(""),
+        leading: new IconButton(
+          icon: new Icon(
+            Icons.arrow_back,
+            // color: Colors.white, size: 30.0,
+          ),
+          color: Colors.black,
+          onPressed: () => Navigator.of(context).pop()
+        ),
+        backgroundColor: Colors.white,
+        // title: new Text("Gym Info"),
+        elevation: 8.0,
+      ),
+      body: new Container(
+        child: new Center(child: new Text("hello"),)
+      ),
+    );
+  }
+}
