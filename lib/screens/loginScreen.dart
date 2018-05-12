db29f642c9b3dbeb043bfe171c795e6874455282
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-05-13 01:24:16 +0530

    Refract code login screen to sync with redux.

--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,82 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart'; 
-import 'package:firebase_auth/firebase_auth.dart';
 import './GymBeam.dart';
-import 'dart:async';    
+import 'dart:async';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import '../redux/reducers.dart'; 
+import '../redux/appState.dart';
+import '../redux/actions.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  LoginScreenState createState() => new LoginScreenState();
-}
+class LoginScreen extends StatelessWidget {
 
-class LoginScreenState extends State<LoginScreen> {
-
-final FirebaseAuth _auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = new GoogleSignIn();
-
-bool loggedIn = false;
-var userDetails;
-
-Future<FirebaseUser> _signIn() async{
-  GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
-
-  FirebaseUser user = await _auth.signInWithGoogle(
-    idToken: gSA.idToken, accessToken: gSA.accessToken
-  );
-
-  print(user.displayName);
-  setState(() {
-      userDetails = user;
-      loggedIn = true;
-    });
-  return user;
-
-}
+final store = new Store(reducers,initialState: new AppState(loggedIn: false));
 
 Widget _loginScreen() {
-  return new MaterialApp(
-      home: new Scaffold(
-        body: new Container(
-          child: new Center(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                    new Container(
-                  width: 290.0,
-                child: new RaisedButton(
-                      elevation: 5.0,
-                      color: Colors.white,
-                      textColor: Colors.black,
-                      onPressed: () => _signIn().then((FirebaseUser user)=>print(user)).catchError((e)=>print(e)),
-                      child: new Row(
-                        children: <Widget>[
-                          new Image.asset("assets/images/google.png",
-                          alignment: Alignment.center,
-                          ),
-                          new Container(
-                            child: new Text("Login with Google",style:new TextStyle(fontSize: 20.0)),
-                            padding: new EdgeInsets.all(10.0),
+  return new Scaffold(
+          body: new Container(
+            child: new Center(
+              child: new Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                      new Container(
+                    width: 290.0,
+                  child: new StoreConnector<dynamic,VoidCallback>(
+                          converter: (store) {
+                            return ()=> store.dispatch(Actions.Login);
+                          },
+                          builder: (context,callback) => new RaisedButton(
+                          elevation: 5.0,
+                          color: Colors.white,
+                          textColor: Colors.black,
+                          onPressed: callback,
+                          child: new Row(
+                            children: <Widget>[
+                              new Image.asset("assets/images/google.png",
+                              alignment: Alignment.center,
+                              ),
+                              new Container(
+                                child: new Text("Login with Google",style:new TextStyle(fontSize: 20.0)),
+                                padding: new EdgeInsets.all(10.0),
+                              )
+                            ],
                           )
-                        ],
-                      )
-                    )
-                ),
-              ],
-            )
-          ),
-        )
-      )
+                        ),
+                  )
+                  ),
+                ],
+              )
+            ),
+          )
     );
 }
 
 Widget _homeScreen() {
-  return new GymBeam(userDetails: userDetails,auth: _auth);
+  return new GymBeam(userDetails: store.state.user,auth: store.state.auth);
 }
 
 
 @override
   Widget build(BuildContext context) {
-    return  loggedIn ? _homeScreen() : _loginScreen();
+    return  new MaterialApp(
+              home: new StoreProvider<dynamic>(
+              store: store,
+              child: new StoreConnector(
+              converter: (store) => store.state.loggedIn,
+              builder: (context,loggedIn)=> loggedIn ? _homeScreen() : _loginScreen(),
+            )
+          )
+    );
   }
 }
