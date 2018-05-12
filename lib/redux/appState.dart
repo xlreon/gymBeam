18a6114cfcf1552397e8459e4a4f850394e55d22
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-05-13 01:23:15 +0530

    Moved login screen logic to reducers.

--- lib/redux/appState.dart
+++ lib/redux/appState.dart
@@ -1,3 +1,11 @@
+import 'package:google_sign_in/google_sign_in.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';  
+
 class AppState {
-  AppState();
+  AppState({this.auth,this.googleSignIn,this.loggedIn,this.user});
+  FirebaseAuth auth;
+  GoogleSignIn googleSignIn;
+  bool loggedIn = false;
+  FirebaseUser user;
 }
--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -1,8 +1,28 @@
 import './appState.dart';
 import './actions.dart';
+import 'package:google_sign_in/google_sign_in.dart'; 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';  
 
 AppState reducers(AppState prev,action) {
   switch(action) {
-    case Actions.Login: print("Login action");
+    case Actions.Login:
+    prev.auth = FirebaseAuth.instance;
+    prev.googleSignIn = new GoogleSignIn();
+    Future<FirebaseUser> signIn() async{
+      GoogleSignInAccount googleSignInAccount = await prev.googleSignIn.signIn();
+      GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+
+      prev.user = await prev.auth.signInWithGoogle(
+      idToken: gSA.idToken, accessToken: gSA.accessToken
+      );
+      print(prev.user);
+      prev.loggedIn = true;
+      print(prev.loggedIn);
+    }
+    signIn();
+    
+
   }
 }
+
