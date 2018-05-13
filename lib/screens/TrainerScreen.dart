8d77e57990e639637a29c8a27ba6b23479bf5e69
Sidharth Satapathy <sidharth.satapathy9@gmail.com>
2018-05-13 21:04:00 +0530

    Refract Trainer Detail screen.

--- lib/screens/TrainerScreen.dart
+++ lib/screens/TrainerScreen.dart
@@ -4,12 +4,8 @@ import 'package:flutter/material.dart';
 import '../components/GymDetailsComponents/gym_api.dart' as api;
 import '../components/GymDetailsComponents/trainer_details_page.dart';
 
-class TrainerDetailsScreen extends StatefulWidget {
-  @override
-  TrainerDetailsState createState() => new TrainerDetailsState();
-}
 
-class TrainerDetailsState extends State<TrainerDetailsScreen> {
+class TrainerDetails extends StatelessWidget {
 
 @override
   Widget build(BuildContext context) {
